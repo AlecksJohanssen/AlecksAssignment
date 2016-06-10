f8f699bb4ca879f543079bc900c00e7ca19fec93
AlecksJohanssen <minh.dq1078@sinhvien.hoasen.edu.vn>
2016-06-10 13:36:06 +0700

    Upgrade Function

--- app/src/main/java/com/example/alecksjohansson/Assignment/MainPage.java
+++ app/src/main/java/com/example/alecksjohansson/Assignment/MainPage.java
@@ -2,14 +2,19 @@ package com.example.alecksjohansson.Assignment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.borax12.materialdaterangepicker.date.DatePickerDialog;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 import it.gmariotti.cardslib.library.internal.Card;
 import it.gmariotti.cardslib.library.internal.CardHeader;
@@ -19,25 +24,24 @@ import it.gmariotti.cardslib.library.view.CardViewNative;
 
 public class MainPage extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     DatePickerDialog dpd;
+
     String date;
-    Calendar[] dates;
+    TextView mtvFlyOut;
+    TextView mTvFlyBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-        Card card = new Card(this);
-        CardHeader header = new CardHeader(getApplicationContext());
-        CardThumbnail thumb = new CardThumbnail(this.getApplicationContext());
-        header.setTitle("Alecks Johanssen");
-        thumb.setDrawableResource(R.drawable.icon_travel);
-        card.addCardThumbnail(thumb);
-        card.addCardHeader(header);
-        CardViewNative cardView = (CardViewNative) findViewById(R.id.list_cardId);
-        cardView.setCard(card);
+        mtvFlyOut = (TextView) findViewById(R.id.tvFlyOut);
+        mTvFlyBack = (TextView) findViewById(R.id.tvFlyBack);
 
 
+    }private void updateFly(String date)
+    {
+        mTvFlyBack.setText(date);
     }
+
     private void setDate()
     {
         Calendar now = Calendar.getInstance();
@@ -47,8 +51,6 @@ public class MainPage extends AppCompatActivity implements DatePickerDialog.OnDa
                 now.get(Calendar.MONTH),
                 now.get(Calendar.DAY_OF_MONTH)
         );
-        dates = new Calendar[13];
-        dpd.setHighlightedDays(dates,dates);
         dpd.setThemeDark(true);
         dpd.show(getFragmentManager(), "Datepickerdialog");
         dpd.setStartTitle("From");
@@ -56,7 +58,10 @@ public class MainPage extends AppCompatActivity implements DatePickerDialog.OnDa
     }
     @Override
     public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth, int yearEnd, int monthOfYearEnd, int dayOfMonthEnd) {
-         date = "You picked the following date: "+dayOfMonth+"/"+(monthOfYear+1)+"/"+year;
+         date = dayOfMonth+"/"+(monthOfYear+1)+"/"+year;
+        if(date != null) {
+            updateFly(date);
+        }
 
     }
 
