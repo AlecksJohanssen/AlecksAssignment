dd2c3f939389eb534f78440e57f8d105dbc33c2a
AlecksJohanssen <minh.dq1078@sinhvien.hoasen.edu.vn>
2016-06-11 11:02:26 +0700

    Upgraded with Metrial Design

--- app/src/main/java/com/example/alecksjohansson/Assignment/MainPage.java
+++ app/src/main/java/com/example/alecksjohansson/Assignment/MainPage.java
@@ -1,5 +1,11 @@
 package com.example.alecksjohansson.Assignment;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,12 +16,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.borax12.materialdaterangepicker.date.DatePickerDialog;
+import com.github.florent37.materialviewpager.MaterialViewPager;
+import com.github.florent37.materialviewpager.header.HeaderDesign;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import it.gmariotti.cardslib.library.internal.Card;
 import it.gmariotti.cardslib.library.internal.CardHeader;
@@ -23,26 +32,96 @@ import it.gmariotti.cardslib.library.internal.CardThumbnail;
 import it.gmariotti.cardslib.library.internal.base.BaseCard;
 import it.gmariotti.cardslib.library.view.CardViewNative;
 
-public class MainPage extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+public class MainPage extends ActionBarActivity implements DatePickerDialog.OnDateSetListener {
     DatePickerDialog dpd;
-
     String dateStart;
     String dateEnd;
     TextView mtvFlyOut;
     TextView mTvFlyBack;
+    private TabLayout mTabLayout;
+    private MaterialViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-        mtvFlyOut = (TextView) findViewById(R.id.tvFlyOut);
-        mTvFlyBack = (TextView) findViewById(R.id.tvFlyBack);
+        //mtvFlyOut = (TextView) findViewById(R.id.tvFlyOut);
+       // mTvFlyBack = (TextView) findViewById(R.id.tvFlyBack);
+        mViewPager = (MaterialViewPager) findViewById(R.id.materialViewPager);
+        mViewPager.getViewPager().setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
 
 
-    }private void updateFly(String dateStart,String dateEnd)
+            @Override
+            public Fragment getItem(int position) {
+                switch (position % 4) {
+                    //case 0:
+                    //    return RecyclerViewFragment.newInstance();
+                    //case 1:
+                    //    return RecyclerViewFragment.newInstance();
+                    //case 2:
+                    //    return WebViewFragment.newInstance();
+                    default:
+                        return RecyclerViewFragment.newInstance();
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 4;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                switch (position % 4) {
+                    case 0:
+                        return "Selection";
+                    case 1:
+                        return "Actualités";
+                    case 2:
+                        return "Professionnel";
+                    case 3:
+                        return "Divertissement";
+                }
+                return "";
+            }
+        });
+
+        mViewPager.setMaterialViewPagerListener(new MaterialViewPager.Listener() {
+            @Override
+            public HeaderDesign getHeaderDesign(int page) {
+                switch (page) {
+                    case 0:
+                        return HeaderDesign.fromColorResAndUrl(
+                                R.color.green,
+                                "https://fs01.androidpit.info/a/63/0e/android-l-wallpapers-630ea6-h900.jpg");
+                    case 1:
+                        return HeaderDesign.fromColorResAndUrl(
+                                R.color.blue,
+                                "http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2014/06/wallpaper_51.jpg");
+                    case 2:
+                        return HeaderDesign.fromColorResAndUrl(
+                                R.color.cyan,
+                                "http://www.droid-life.com/wp-content/uploads/2014/10/lollipop-wallpapers10.jpg");
+                    case 3:
+                        return HeaderDesign.fromColorResAndUrl(
+                                R.color.red,
+                                "http://www.tothemobile.com/wp-content/uploads/2014/07/original.jpg");
+                }
+
+                //execute others actions if needed (ex : modify your header logo)
+
+                return null;
+            }
+        });
+
+        mViewPager.getViewPager().setOffscreenPageLimit(mViewPager.getViewPager().getAdapter().getCount());
+        mViewPager.getPagerTitleStrip().setViewPager(mViewPager.getViewPager());
+    }
+
+    private void updateFly(String dateStart,String dateEnd)
     {
-        mTvFlyBack.setText(dateEnd);
-        mtvFlyOut.setText(dateStart);
+       // mTvFlyBack.setText(dateEnd);
+      //  mtvFlyOut.setText(dateStart);
     }
 
     public void setDate(View view)
